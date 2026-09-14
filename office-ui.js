a99aa960aac19dbb9b31972743f73cd40c1e1d9a
melchior1000 <melchior1000@hotmail.com>
2026-09-14 14:37:26 -0300

    feat: liga Recepção ao Cadastro
    Permite transformar um atendimento da Recepção em caso real no Cadastro, com ID idempotente, bloqueio documental e arquivamento da fila somente após a criação confirmada.

--- office-ui.js
+++ office-ui.js
@@ -123,3 +123,4 @@ document.write('<script src="login-theme.js"><\/script>');
 document.write('<script src="office-flow-ui.js"><\/script>');
 document.write('<script src="lib/office-command.js"><\/script>');
 document.write('<script src="office-command-ui.js"><\/script>');
+document.write('<script src="reception-handoff-ui.js"><\/script>');
